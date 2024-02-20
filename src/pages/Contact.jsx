8bcc1774ae8db78e1d74e2e1aaac7d3fb53f64cc
Cindy Fang <cindy.fang@torontomu.ca>
2024-02-20 10:58:30 -0500

    initial commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -41,8 +41,7 @@ const Contacts = () => {
             //show error message
         })
     };
-
-
+    
     return (
     <section className="relative flex lg:flex-row flex-col max-container">
         <div className="flex-1 min-w-[50%] flex flex-col">
